bc686a77a4999fe317bb97fb9ee5d4478c993bb8
kamiada <kamiada528@gmail.com>
2021-10-27 12:27:24 +0100

    is it a manic episode?

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -5,8 +5,8 @@ class Panel extends Component {
   render() {
     return (
 
-      <button className="parent_hexagon" onClick={this.props.onClick}>
-        <img className="hexagon" src={this.props.image} alt={this.props.alt}/>
+      <button className="parent_square" onClick={this.props.onClick}>
+        <img className="square" src={this.props.image} alt={this.props.alt}/>
       </button>
     );
   }
