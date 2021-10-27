013ffff47d76b4f61875fb4f7f607a0534bf3df6
kamiada <kamiada528@gmail.com>
2021-10-27 15:42:54 +0100

    cannot decide

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import Panel from './Panel.jsx';
 import Menu from './Menu.jsx';
 import Layout from './Layout.jsx';
-import Tile from './Tile.jsx';
-export { Panel, Menu, Layout, Tile };
+export { Panel, Menu, Layout };
--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./works.scss";
-import { Layout, Tile } from "../../components";
+import { Layout } from "../../components";
 
 const work = () => {
   return (
     <Layout>
         <div className="work_container" id="work">
         <div className="holder">
-        <Tile 
-          image="nanan"
-          text="batman"
-        />
+
+          <div className="tile">
+            <img className="tile_img" src="" alt=""/>
+            <div className="tile_text">Example text</div>
+          </div>
+
+          <div className="tile">
+          <div className="tile_text">nanana</div>
+            <img className="tile_img" src="" alt=""/>
+          </div>
+
         </div>
       </div>
     </Layout>
