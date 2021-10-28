8f9bd62bb2e5371cc361e4e5aab074a0a76c1101
kamiada <kamiada528@gmail.com>
2021-10-28 07:38:36 +0100

    it looks nice

--- src/images/index.js
+++ src/images/index.js
@@ -1,4 +1,6 @@
 import NHM from './NHM.png';
 import profilePic from './profile.png';
 import photo from './IMG_0001.JPG';
-export { NHM, profilePic, photo };
+import Github from './GitHub-Mark-Light-64px.png';
+import Linkedin from './whiteLinkedin.png';
+export { NHM, profilePic, photo, Github, Linkedin };
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./contact.scss";
 import { Layout } from "../../components";
+import {Github, Linkedin} from '../../images';
 
 const contact = () => {
   return (
     <Layout>
       <div id="contact" className="contact_container">
-        <footer className="container">hjj</footer>
+          <div className="link_container">
+            <img src={Github} alt="github icon"/>
+          </div>
+          <div className="link_container">
+            <img src={Linkedin} alt="linkedin icon"/>
+          </div>
+          <footer className="container">
+          <span>&#169;</span> Adrianna Kaminska
+        </footer>
       </div>
     </Layout>
   );
