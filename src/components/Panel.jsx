6babb64e034d4be6db31c98bdbf525f1fef0c94d
kamiada <kamiada528@gmail.com>
2021-11-01 19:32:52 +0000

    sad sad sad

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -4,7 +4,6 @@ import "./components.scss";
 class Panel extends Component {
   render() {
     return (
-
       <button className="parent_square" onClick={this.props.onClick}>
         <img className="square" src={this.props.image} alt={this.props.alt}/>
       </button>
