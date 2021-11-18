1fffec0e9f8ce68eefeba440753e1d1a361abe4a
kamiada <kamiada528@gmail.com>
2021-11-18 16:44:04 +0000

    making popup

---
+++ src/components/Popup.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./components.scss";
+
+class Popup extends Component {
+  render() {
+    return (
+      <div>
+          testing
+      </div>
+    );
+  }
+}
+
+export default Popup;
