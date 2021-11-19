17b24cee88c86e7e196a1113f3739f0b7c5737b5
kamiada <kamiada528@gmail.com>
2021-11-19 14:18:16 +0000

    boiler plate

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,11 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./components.scss";
 
 class Popup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            itsActive: false,
+        }
+        this.derenderSubpagelta = this.renderSubpage.bind(this);
+        this.closeSubpage = this.closeSubpage.bind(this);
+    }
+    renderSubpage = () => {
+        this.setState({
+            itsActive: true,
+        });
+    }
+    closeSubpage = () => {
+      this.setState({
+        itsActive: false,
+      });
+    }
   render() {
     return (
       <div>
-          testing
+        <button onClick={this.renderSubpage} className="smallTitle">
+          <h2>about.</h2>
+        </button>
+        <Fragment>
+        <div className={this.state.itsActive ? 'miniPage--active': 'miniPage'} >
+            <p>The purpose of this page is to present in graph data regarding COVID-19 in U.K, specifically Scotland</p>
+            <button onClick={this.closeSubpage} className="btnClose">
+            <img className="image" src={Cross} alt="Cross by Aybige from the Noun Project" />
+            </button>
+        </div>
+      </Fragment>
       </div>
     );
   }
