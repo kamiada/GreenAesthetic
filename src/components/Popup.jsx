adbad8662f4ddffb53a1ca8cd66d0e38baf2080d
kamiada <kamiada528@gmail.com>
2021-11-20 09:59:57 +0000

    tiny

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -30,7 +30,7 @@ class Popup extends Component {
         <div className={this.state.itsActive ? 'miniPage--active': 'miniPage'} >
             <p>The purpose of this page is to present in graph data regarding COVID-19 in U.K, specifically Scotland</p>
             <button onClick={this.closeSubpage} className="btnClose">
-            <img className="image" src={Cross} alt="Cross by Aybige from the Noun Project" />
+            <img className="image"  alt="Cross by Aybige from the Noun Project" />
             </button>
         </div>
       </Fragment>
