e406666e1731032598a829a15963c98d34c861d8
kamiada <kamiada528@gmail.com>
2021-11-24 09:17:21 +0000

    smol snake

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -3,15 +3,26 @@ import "./components.scss";
 import { Popup } from ".";
 
 class Panel extends Component {
+  openPopup = (desc) => {
+    if(desc) {
+      
+    }
+  }
   render() {
     return (
       <button className="parent_square" onClick={this.props.onClick}>
         <img className="square" src={this.props.image} alt={this.props.alt} />
         <div className="overlay">
-          <div class="sliding_text">
+          <div class="sliding_text"> 
+          {
+            this.props.link ? 
             <a className="details_holder" href={this.props.link}>
               {this.props.projectTitle}
             </a>
+            :
+            <button onClick={()=>this.openPopup(this.props.description)}>{this.props.projectTitle}</button>
+          }
+           
             {this.props.tags &&
               this.props.tags.map((tag) => {
                 return (
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -5,15 +5,13 @@ import "./projects.scss";
 import projects from "./projects.json";
 
 const displayDetails = () => {
-  console.log('works');
+  console.log("works");
 };
 
 const Projects = () => {
   return (
     <Layout>
-      <div className="section_title_projects">
-        My Projects
-      </div>
+      <div className="section_title_projects">My Projects</div>
       <div className="projects-container" id="projects">
         <div className="table">
           <Panel
@@ -30,7 +28,6 @@ const Projects = () => {
             projectTitle={projects.projectsDescriptions[1].title}
             link={projects.projectsDescriptions[1].github}
             tags={projects.projectsDescriptions[1].tags}
-
           />
           <Panel
             image={Live}
@@ -38,22 +35,20 @@ const Projects = () => {
             projectTitle={projects.projectsDescriptions[2].title}
             link={projects.projectsDescriptions[2].github}
             tags={projects.projectsDescriptions[2].tags}
-
           />
           <Panel
             image={MobileApp}
             alt="Poster of project for the dissertation"
             projectTitle={projects.projectsDescriptions[3].title}
-            link={projects.projectsDescriptions[3].outsideLinks}
+            // link={projects.projectsDescriptions[3].outsideLinks}
+            description={projects.projectsDescriptions[3].post}
             tags={projects.projectsDescriptions[3].tags}
-
           />
           <Panel
             image={NHM}
             alt="Redesign for the website of Natural History Museum"
             projectTitle={projects.projectsDescriptions[4].title}
             tags={projects.projectsDescriptions[4].tags}
-
           />
           <Panel
             image={NHM}
@@ -61,7 +56,6 @@ const Projects = () => {
             projectTitle={projects.projectsDescriptions[5].title}
             link={projects.projectsDescriptions[5].github}
             tags={projects.projectsDescriptions[5].tags}
-
           />
           <Panel
             image={Arctica}
@@ -69,15 +63,13 @@ const Projects = () => {
             projectTitle={projects.projectsDescriptions[6].title}
             link={projects.projectsDescriptions[6].links}
             tags={projects.projectsDescriptions[6].tags}
-
           />
-                    <Panel
+          <Panel
             image={NHM}
             alt="Redesign for the website of Natural History Museum"
             projectTitle={projects.projectsDescriptions[7].title}
             link={projects.projectsDescriptions[7].github}
             tags={projects.projectsDescriptions[7].tags}
-
           />
         </div>
       </div>
